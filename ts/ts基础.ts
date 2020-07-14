e46eb0065890fb8b514005cc5cce369e5401a2b5
cxpJYF <2256843428@qq.com>
2020-07-14 12:49:40 +0800

    ts基础.ts 增加 枚举 更改下标文档

--- ts/ts基础.ts
+++ ts/ts基础.ts
@@ -4,30 +4,39 @@
 
 /** 数据类型 */ 
 // 基础数据类型 string boolean number 枚举 any void undefined null
-    let email:string = "2256@qq.com"; // 字符串
-    let msg:string = `my email is ${email}`; // 模板字符串
-    let isShow:boolean = false; // 布尔
-    let age:number = 20; // 数字 age="19"会报错，因为之前定义的是数字类型
-    let list:number[]=[1,2,3,4]; // 数组 里面的值必须是number类型
-    let list2:string[]=["a","b","c"]; // 数组 里面的值必须是string类型
-    let list3:[string,number]=["a",10]; // 元祖 第一个是string,第二个是number
-    // 枚举 值必须是列举出来的范围内
-    enum Weeks{Mon,Tue,Wed}; // enum 枚举集{枚举值1}
-    let day:Weeks = Weeks.Tue; // 变量名:枚举名 = 枚举集里的值
-    day; // 0
-    Weeks[0]; // Mon
-    Weeks["Mon"]; // 0
-    // Any 任何类型都可以
+// string
+    let email: string = "2256@qq.com"; // 字符串
+    let msg: string = `my email is ${email}`; // 模板字符串
+// boolean
+    let isShow: boolean = false; // 布尔
+// number 都是浮点数,支持十进制,8进制,16进制,以及字面量
+    let age: number = 20; // 数字 age="19"会报错，因为之前定义的是数字类型
+// 数组
+    let list: number[]=[1,2,3,4]; // 数组 里面的值必须是number类型
+    let list2: string[]=["a","b","c"]; // 数组 里面的值必须是string类型
+    let list3: Array<number> = [1,2,3];// 数组泛型方式创建数组
+// 元祖 表示一个已知元素数量和类型的数组
+    let list4:[string,number]=["a",10]; // 第一个是string,第二个是number,数量和类型要已知
+    // list4[1].substr(1); 报错,因为第二项是数字,无字符串方法
+// 枚举 enum 值必须是列举出来的范围内
+    enum Weeks{Mon,Tue = 4,Wed}; // enum 枚举名{枚举值...} 可以改变值得下标
+    let day:Weeks = Weeks.Mon; // 变量名:枚举名 = 枚举集里的值
+    day; // 0 此处返回的是Mon的下标
+    Weeks[0]; // Mon 具体的值
+    Weeks["Mon"]; // 0 下标
+    Weeks["Tue"]; // 4 下标 之前就把下标改了
+    Weeks["Wed"]; // 5 下标 前一个下标被改成了4，故它顺延为5
+// Any 任何类型都可以
     let any1:any= 1;
     let any2:any= true;
-    // void 无任何类型，一般是函数返回值
+// void 无任何类型，一般是函数返回值
     function hello():void{
         // 这个函数就不能再return一个返回值了，因为之前定义了无返回类型
     }
     function hello2():string{
         return `这里必须返回字符串,因为之前定义了`
     }
-    // undefined null 一般不用
+// undefined null 一般不用
     let und1:undefined = undefined // 值就只能是undefined
     let nud1:null = null; // 值只能是null
 // 联合类型
--- ts/接口-类.ts
+++ ts/接口-类.ts
@@ -1,48 +1,48 @@
 /** 接口 interface */
 // 接口定义 约束key名和value数据类型
-interface Person{
-    firstName: string; // key一定要是firstName,值一定要是string类型
-    getName(msg:string):string; // 一定要有一个方法是getName,参数一定是string,返回值一定是string
-}
-function fun2(val: Person){ // 参数注解，按照Person接口来约束
-    return `参数只要符合Person接口就行${val.firstName} ${val.getName}`;
-}
-fun2({firstName: "Jane", getName(msg:string){return msg} }) //传参的时候,要按照约束来
+    interface Person{
+        firstName: string; // key一定要是firstName,值一定要是string类型
+        getName(msg:string):string; // 一定要有一个方法是getName,参数一定是string,返回值一定是string
+    }
+    function fun2(val: Person){ // 参数注解，按照Person接口来约束
+        return `参数只要符合Person接口就行${val.firstName} ${val.getName}`;
+    }
+    fun2({firstName: "Jane", getName(msg:string){return msg} }) //传参的时候,要按照约束来
 
 // 对函数进行约束
-interface Imyfunction{
-    (a:string,b:number):boolean; // 对参数和返回值约束
-}
-let ys1:Imyfunction;
-ys1 = function(a:string,b:number){
-    return false;
-}
+    interface Imyfunction{
+        (a:string,b:number):boolean; // 对参数和返回值约束
+    }
+    let ys1:Imyfunction;
+    ys1 = function(a:string,b:number){
+        return false;
+    }
 // 对数组进行约束
-interface Istuarr{
-    [index:number]:string; // 值必须是字符串
-}
-let ys2:Istuarr;
-ys2 = ["aaa","bbb"];
+    interface Istuarr{
+        [index:number]:string; // 值必须是字符串
+    }
+    let ys2:Istuarr;
+    ys2 = ["aaa","bbb"];
 // json
-interface Idata{ 
-    name:string, // 对key名和value数据类型进行约束
-    readonly age:number // 只读属性 不能修改
-    email?:string // 可选属性，不一定要传
-}
-function showData(n:Idata){
-    console.log(n)
-}
-showData({name:"a",age:19})
+    interface Idata{ 
+        name:string, // 对key名和value数据类型进行约束
+        readonly age:number // 只读属性 不能修改
+        email?:string // 可选属性，不一定要传
+    }
+    function showData(n:Idata){
+        console.log(n)
+    }
+    showData({name:"a",age:19})
 
 /** 类 */
 // 跟es6相识
-class Student{
-    funllName: string;
-    // 使用public等于创建了同名的成员变量
-    constructor(public firstName, public mid, public getName){
-        this.funllName = firstName + mid + getName;
+    class Student{
+        funllName: string;
+        // 使用public等于创建了同名的成员变量
+        constructor(public firstName, public mid, public getName){
+            this.funllName = firstName + mid + getName;
+        }
     }
-}
-let user = new Student("jane","M.","User"); // 新建一个类实例
-fun2(user); // 类可以和接口一起使用,把实例传到函数,接口会对实例(参数)进行约束
+    let user = new Student("jane","M.","User"); // 新建一个类实例
+    fun2(user); // 类可以和接口一起使用,把实例传到函数,接口会对实例(参数)进行约束
 // class 类名 implements 接口1 接口2{...} 可以用接口来约束类，而且可以同时使用多个接口
