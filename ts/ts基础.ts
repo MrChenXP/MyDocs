43bd292f7b4ad912b475978d5b0bb9e24b39f53f
cxpJYF <2256843428@qq.com>
2020-07-14 18:46:28 +0800

    ts基础.ts 增加never类型

--- ts/ts基础.ts
+++ ts/ts基础.ts
@@ -13,7 +13,7 @@
     let age: number = 20; // 数字 age="19"会报错，因为之前定义的是数字类型
 // 数组
     let list: number[]=[1,2,3,4]; // 数组 里面的值必须是number类型
-    let list2: string[]=["a","b","c"]; // 数组 里面的值必须是string类型
+    let list2: any[]=["a","b","c"]; // 数组 里面的值所有类型都可以
     let list3: Array<number> = [1,2,3];// 数组泛型方式创建数组
 // 元祖 表示一个已知元素数量和类型的数组
     let list4:[string,number]=["a",10]; // 第一个是string,第二个是number,数量和类型要已知
@@ -26,19 +26,40 @@
     Weeks["Mon"]; // 0 下标
     Weeks["Tue"]; // 4 下标 之前就把下标改了
     Weeks["Wed"]; // 5 下标 前一个下标被改成了4，故它顺延为5
-// Any 任何类型都可以
+    let day2:string = Weeks[4]; // 可以直接用下标赋值
+// any 任何类型都可以
     let any1:any= 1;
     let any2:any= true;
 // void 无任何类型，一般是函数返回值
     function hello():void{
-        // 这个函数就不能再return一个返回值了，因为之前定义了无返回类型
+        // 这个函数就不能再return一个返回值了,因为之前定义了返回值为void
     }
-    function hello2():string{
-        return `这里必须返回字符串,因为之前定义了`
-    }
-// undefined null 一般不用
+    let uns1: void = undefined; // 一半void没啥用,因为只能赋值undefined,null
+// undefined null 二者是任何类型的子类型,可以赋值给任何类型
     let und1:undefined = undefined // 值就只能是undefined
-    let nud1:null = null; // 值只能是null
+    let nud2:null = null; // 值只能是null
+    let nud3:string = null; // 子类型 --strictNullChecks可以避免这个情况
+// never 永不存在的类型
+    // 1.总是会抛出异常或根本就不会有返回值 的 函数表达式或箭头函数表达式 的 返回值类型
+    // 2.变量:被永不为真的类型保护所约束时
+    // 3.never类型是任何类型的子类型,
+    // 4.没有类型是never的子类型或可以赋值给never类型(除了never本身之外)(any也不行)
+    // 返回never的函数必须存在无法达到的终点
+    function error(message: string): never {
+        throw new Error(message);
+    }
+    // 推断的返回值类型为never
+    function fail() {
+        return error("Something failed");
+    }
+    // 返回never的函数必须存在无法达到的终点
+    function infiniteLoop(): never {
+        while (true) {
+        }
+    }
+// object 非原始类型
+    let ojb1: object = {pro: 0};
+
 // 联合类型
     let l1:number | string | boolean; // 可以是数字 布尔 字符串
 // 类型推论 
@@ -50,7 +71,7 @@
     let l5:number | string | boolean = "this is a string";
     let strLen:number = l5.length; // l5已经被赋值成了字符串类型
     let strLen2:number = (<string>l5).length; // 将l5断言为string类型,
-    let strLen3:number = (l5 as string).length; // as写法，将l5断言为string类型
+    let strLen3:number = (l5 as string).length; // 将l5断言为string类型 jsx中,只有这种as写法可以
 // 类型注解 为函数或变量添加约束
     function fun1(person: string){
         return `传进来的参数只能是string`;
