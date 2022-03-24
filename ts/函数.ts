03d0254c7e59550de36a925cc24de5b57908c004
CXP <2256843428@qq.com>
2022-03-25 01:04:16 +0800

    ts函数文档添加

--- ts/函数.ts
+++ ts/函数.ts
@@ -1,20 +1,27 @@
-/** 函数 */
-// 定义
-    function add(x: number, y: number): number{
+/** ===========函数=========== */
+// 定义 实际给函数的参数和返回值加类型
+    function add(x: number, y: number): number {
         return x + y;
     }
-    // 默认参数 ?可选参数(一般放在末尾)  (一个参数不能同时是默认和可选)
-    let add1 = function(x: number = 20, y?: number): void{ // 匿名函数
+// 默认参数 ?可选参数  匿名函数
+    let add1 = function (x: number = 20, y?: number): void {
+        // 一个参数不能同时是默认和可选
+        // 可选参数只能出现在必选参数后面
+        // 匿名函数 空返回值类型
         x + y; // 无返回类型,所以不能return
-    }
-    // 剩余参数
-    let add2 = function(x: number, ...arr: number[]){
+    };
+// 剩余参数
+    let add2 = function (x: number, ...arr: number[]) {
         arr; // [2,3,4,5]
-    }
-    let sum = add2(1,2,3,4,5)
-// 函数的重载
-    // 相当于重名名函数，后面的覆盖前面的
+    };
+// 将函数声明交给变量进行声明
+    let add3: (x: number, y: string) => number = (x, y) => {
+        return x;
+    };
+    let sum = add2(1, 2, 3, 4, 5);
 
+// 函数的重载
+// 相当于重名名函数，后面的覆盖前面的
 
 // declare var 声明全局变量
 // declare function 声明全局方法
