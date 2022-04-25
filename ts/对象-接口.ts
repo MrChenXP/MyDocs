83959e776c6da3696a8437eb90393ec505d73d3c
cxpJYF <2256843428@qq.com>
2022-04-25 19:53:13 +0800

    ts整理

--- ts/对象-接口.ts
+++ ts/对象-接口.ts
@@ -1,44 +1,69 @@
 /** =========对象========= */
 // 描述对象的结构：有什么类型的属性和方法
-let person: { name: string } = { name: "jack" };
-
+    let person: { name: string } = { name: "jack" };
 // 通过自定义类型 创建一个对象类型
-type objType = {
-  name: string;
-  age?: number; // 可选属性
-  sayHi(): void;
-  greet(name: string): void;
-  sayAge(age: number): number;
-  sayName: () => void;
-  getName: Function;
-};
+    type objType = {
+    name: string;
+    age?: number; // 可选属性
+    sayHi(): void;
+    greet(name: string): void;
+    sayAge(age: number): number;
+    sayName: () => void;
+    getName: Function;
+    };
+    let myObj = (config: objType): void => {}; // 将创建好的类型使用在函数定义上
 
-let myObj = (config: objType): void => {}; // 将创建好的类型使用在函数定义上
-
-/** =========接口 interface========= */
-// 当一个对象类型被多次使用时,一般会使用接口interface来描述对象的类型,打到复用的目的
-interface IPerson {
-  name: string;
-  age?: number; // 可选属性
-  sayHi?(): void;
-  greet?(name: string): void;
-  sayAge?(age: number): number;
-  sayName?: () => void;
-  getName?: Function;
-}
-let myObj2: IPerson = { name: "jack" };
-// 接口interface和类型别名type的对比
-// -都可以给对象指定类型
-// *接口，只能为对象指定类型
-// *类型别名，不仅可以给对象指定类型，实际可以给任意类型制定别名
-
+/** =========接口interface========= */
+// 接口定义 约束key名和value数据类型 给对象用的
+    interface Person {
+        name?: string, // ? 可选属性
+        readonly age?: string, // readonly 只读属性
+        firstName: string; // key一定要是firstName,值一定要是string类型
+        getName(msg: string): string; // 一定要有一个方法是getName,参数一定是string,返回值一定是string
+        getAge?: ()=>void;
+        getFirstName?: Function;
+    }
+    function fun2(val: Person) {
+        // 参数注解，按照Person接口来约束
+        return `参数只要符合Person接口就行${val.firstName} ${val.getName}`;
+    }
+    fun2({
+        firstName: 'Jane',
+        getName(msg: string) {
+            return msg;
+        },
+    }); //传参的时候,要按照约束来
+// 对函数进行约束
+    interface ImyFunction {
+        (a: string, b: number): boolean; // 对参数和返回值约束
+    }
+    let ys1: ImyFunction;
+    ys1 = function (a: string, b: number) {
+        return false;
+    };
+// 对数组进行约束
+    interface IStatus {
+        [index: number]: string; // 值必须是字符串
+    }
+    let ys2: IStatus;
+    ys2 = ['aaa', 'bbb'];
+// json
+    interface IData {
+        name: string; // 对key名和value数据类型进行约束
+        readonly age: number; // 只读属性 不能修改
+        email?: string; // 可选属性，不一定要传
+    }
+    function showData(n: IData) {
+        // console.log(n) // name:a age:19
+    }
+    showData({ name: 'a', age: 19 });
 // 继承 extends
-// 如果有相同的属性和方法,可以将公共的属性或方法抽离出来,通过继承实现复用
-interface Point2D {
-  x: number;
-  y: number;
-}
-interface Point3D extends Point2D {
-  z: number;
-}
-let myObj3: Point3D = { x: 1, y: 2, z: 3 };
+    // 如果有相同的属性和方法,可以将公共的属性或方法抽离出来,通过继承实现复用
+    interface Point2D {
+        x: number;
+        y: number;
+    }
+    interface Point3D extends Point2D {
+        z: number;
+    }
+    let myObj3: Point3D = { x: 1, y: 2, z: 3 };
--- ts/索引-映射.ts
+++ ts/索引-映射.ts
@@ -1,5 +1,29 @@
-// 映射类型
-  // 基于旧类型创建新类型(对象类型)
+/** ===========索引签名类型=========== */ 
+  // 当无法确定对象中有哪些属性，或者对象可以出现任意多个属性
+  // [key: string]来约束属性名称
+  // obj可以出现任意多个属性
+  // key只是个占位符,可以任意变量名称
+  // js对象({})的键是string类型
+  interface AnyObject {
+    [key: string]: number
+  }
+  let obj: AnyObject = {
+    a: 1,
+    b: 2,
+    2: 3,
+  }
+// 数组
+  // js中数组是特殊对象,数组的键(索引)是数值类型
+  // 模拟原生的数组接口,并使用[n: number]来作为索引签名类型
+  // 只要number类型的键(索引)都可以出现在数组中,或者说数组可以有任意多个元素
+  // 同时也符合数组索引时number类型这一前提
+  interface MyArray<T> {
+    [n: number]: T
+  }
+  let arr: MyArray<number> = [1, 2, 3] 
+
+/** ===========映射类型=========== */ 
+// 基于旧类型创建新类型(对象类型)
   // in
   type PropKeys = 'x' | 'y' | 'z'
   type Type2 = { [Key in PropKeys]: number }
@@ -22,7 +46,7 @@
   // : 后面T[P]表示获取T每个键对应的类型（abc）
   // 最终新旧类型结构完全相同 只是变成可选了
 
-// 索引查询(访问)类型
+/** ===========索引查询(访问)类型=========== */ 
   // 用来查询属性的类型 像上面的T[P]
   type Props3 = { a: number; b: string; c: boolean}
   type TypeA = Props3['a'] // TypeA = number
