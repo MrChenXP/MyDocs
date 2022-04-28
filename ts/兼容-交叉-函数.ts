d2b59e42a032bc8375a8e20f2f18c82d812b9f70
cxpJYF <2256843428@qq.com>
2022-04-28 19:55:37 +0800

    ts整理

--- ts/兼容-交叉-函数.ts
+++ ts/兼容-交叉-函数.ts
@@ -56,3 +56,25 @@
   // interface B extends A { a: string} 报错 a类型不兼容
   interface B { a: string}
   type C = A & B // 不报错 理解为 C = { a: number | string }
+
+/** ===========函数=========== */
+// 定义 实际给函数的参数和返回值加类型
+  function add(x: number, y: number): number {
+    return x + y;
+  }
+// 默认参数 ?可选参数  匿名函数
+  let add1 = function (x: number = 20, y?: number): void {
+    // 一个参数不能同时是默认和可选
+    // 可选参数只能出现在必选参数后面
+    // 匿名函数 空返回值类型
+    x + y; // 无返回类型,所以不能return
+  };
+// 剩余参数
+  let add2 = function (x: number, ...arr: number[]) {
+    arr; // [2,3,4,5]
+  };
+// 将函数声明交给变量进行声明
+  let add3: (x: number, y: string) => number = (x, y) => {
+    return x;
+  };
+  let sum = add2(1, 2, 3, 4, 5);
