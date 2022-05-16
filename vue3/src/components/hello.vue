2a65377e1fac70a5bef58e6773ba605402803bbe
CXP <2256843428@qq.com>
2022-05-17 01:49:48 +0800

    vue3 代理对象

--- vue3/src/components/hello.vue
+++ vue3/src/components/hello.vue
@@ -71,7 +71,23 @@ export default defineComponent({
     //   文档:
     //   https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Proxy
     //   https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Reflect
+    new Proxy(obj, {
+      // 拦截读取属性值
+        get (target, prop) {
+          return Reflect.get(target, prop)
+        },
+        // 拦截设置属性值或添加新属性
+        set (target, prop, value) {
+          return Reflect.set(target, prop, value)
+        },
+        // 拦截删除属性
+        deleteProperty (target, prop) {
+          return Reflect.deleteProperty(target, prop)
+        }
+    })
 
+    // proxy.name = 'tom'   
+    
     }
   });
 </script>
