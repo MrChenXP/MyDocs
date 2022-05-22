63477b00f4593b1dd2b02a6be3cc5d09ecf50a40
CXP <2256843428@qq.com>
2022-05-23 00:55:33 +0800

    vue3 生命周期

---
+++ vue3/src/components/生命周期.js
@@ -0,0 +1,17 @@
+// 与 2.x 版本生命周期相对应的组合式 API
+
+// beforeCreate -> 使用 setup()
+// created -> 使用 setup()
+// beforeMount -> onBeforeMount
+// mounted -> onMounted
+// beforeUpdate -> onBeforeUpdate
+// updated -> onUpdated
+// beforeDestroy -> onBeforeUnmount
+// destroyed -> onUnmounted
+// errorCaptured -> onErrorCaptured
+// 新增的钩子函数
+
+// 组合式 API 还提供了以下调试钩子函数：
+
+// onRenderTracked
+// onRenderTriggered
