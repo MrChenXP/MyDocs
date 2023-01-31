6ee2cac64d536f56a71c5a7372007832b92862e4
cxpJYF <2256843428@qq.com>
2023-01-31 15:20:30 +0800

    husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
-// linlint-staged 要运行的命令 不能为空，设置了什么lint就得装什么lint包
+// linlint-staged要运行的命令 不能为空,设置了什么lint就得配置好
 module.exports = {
   '*.vue': ['stylelint --cache --fix --allow-empty-input', 'eslint --cache --fix'],
   '*.{scss,css}': 'stylelint --cache --fix --allow-empty-input',
